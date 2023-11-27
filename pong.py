36749aa2b2b4ce472cdf6804010be6b0c7fad605
Electro3020 <henrybartholomew@icloud.com>
2023-11-27 01:29:19 +0000

    Fixed collision and added "disabled" attribute

--- pong.py
+++ pong.py
@@ -13,6 +13,7 @@ from pygame.locals import (
     K_LEFT,
     K_RIGHT,
     K_ESCAPE,
+    K_SPACE,
     KEYDOWN,
     QUIT,
 )
@@ -39,79 +40,81 @@ BLUE = (0, 0, 255)
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # Number of balls to create
-NUM_BALLS = 1
+NUM_BALLS = 200
 
 # Ball dimensions
-BALL_RADIUS = 8
+BALL_RADIUS = 16
 
 # Ball start position
 BALL_START_X = 960
 BALL_START_Y = 540
 
 # Ball velocity
-BALL_VELOCITY = 8
-BALL_VEL_X = BALL_VELOCITY
-BALL_VEL_Y = BALL_VELOCITY
+BALL_VELOCITY = 5
 
-PADDLE_HEIGHT = 100
-PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 200
+PADDLE_WIDTH = 20
 PADDLE_VELOCITY = 15
 
 
 class Ball():
     def __init__(self, colour, radius, pos_x, pos_y, vel_x, vel_y):
         self.radius = radius
-        self.rect = pygame.draw.circle(screen, colour, (pos_x, pos_y), radius)
         self.colour = colour
         self.pos_x, self.pos_y = pos_x, pos_y
         self.vel_x = vel_x
         self.vel_y = vel_y
-        print("Created ball")
+        self.collided = False
+        self.disabled = False
+        self.update()
     
     def update(self):
         #print(f"{self.pos_x}, {self.pos_y}")
         #self.rect = pygame.draw.circle(screen, self.colour, (self.pos_x, self.pos_y), self.radius)
-        self.rect.move(self.pos_x,self.pos_y)
+        self.rect = pygame.draw.circle(screen, self.colour, (self.pos_x, self.pos_y), self.radius)
 
-class Paddle(pygame.Rect):
-    def __init__(self, id, pos_x, pos_y, height, width, velocity):
-        self.id = id
+class Paddle():
+    def __init__(self, pos_x, pos_y, height, width, velocity):
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.colour = WHITE
         self.height = height
         self.width = width
         self.velocity = velocity
+        self.update()
         # rect = pygame.draw.rect(screen, (colourRGB), (ball_x, ball_y, width, height)
-        self.rect = pygame.draw.rect(screen, WHITE, (pos_x, pos_y, 10, 100))
     
     def update(self):
         #self.rect = pygame.draw.rect(screen, WHITE, (self.pos_x, self.pos_y, 10, 100))
-        self.rect.move(self.pos_x,self.pos_y)
+        self.rect = pygame.draw.rect(screen, WHITE, (self.pos_x, self.pos_y, self.width, self.height))
 
-# paddle_b_x = (SCREEN_WIDTH / 30) * 29
-# paddle_b_y = SCREEN_HEIGHT / 2
+def spawn_ball(i):
+    print(f"Created ball at index {i}")
+    balls.append(
+        Ball(WHITE, BALL_RADIUS, BALL_START_X, BALL_START_Y, (BALL_VELOCITY + random.randint(0,i))*(1 if i%2 == 0 else -1), BALL_VELOCITY/5 + random.randint(-20,20))
+    )
 
 # Scoring system
 score = 0
-paddle_a = Paddle(1, SCREEN_WIDTH / 30, SCREEN_HEIGHT / 2, PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_VELOCITY)
-paddle_b = Paddle(2, (SCREEN_WIDTH / 30) *29, SCREEN_HEIGHT / 2, PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_VELOCITY)
 
-#ball = Ball(WHITE, BALL_RADIUS, BALL_START_X, BALL_START_Y, BALL_VEL_X, BALL_VEL_Y)
+
+paddle_a = Paddle(SCREEN_WIDTH / 30, SCREEN_HEIGHT / 2, PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_VELOCITY)
+paddle_b = Paddle((SCREEN_WIDTH / 30) *29, SCREEN_HEIGHT / 2, PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_VELOCITY)
 
 # Create balls
 balls = []
 for i in range(NUM_BALLS):
     # Add ball to list
-    balls.append(
-        Ball(WHITE, BALL_RADIUS, BALL_START_X, BALL_START_Y, i+5, 1 if i%2 == 0 else -1))
+    spawn_ball(i)
 
+space_key_pressed = False
 # Run until the user asks to quit
 pygame.display.set_caption("Pong Test Game")
-
+pygame.display.update()
 running = True
-screen.fill(BLACK)
 while running:
     clock.tick(FPS)
+    screen.fill(BLACK)
 
     # Game close mechanic
     for event in pygame.event.get():
@@ -131,20 +134,35 @@ while running:
     if keys[pygame.K_DOWN] and paddle_b.pos_y < 1080 - paddle_a.height:
         paddle_b.pos_y += paddle_b.velocity
 
-    # Move ball
+    if keys[pygame.K_SPACE]:
+        if not space_key_pressed:
+            spawn_ball(len(balls))
+            space_key_pressed = True
+    else:
+        space_key_pressed = False
 
-    # Ball border detection
     for ball in balls:
-        ball.pos_x += ball.vel_x
-        ball.pos_y += ball.vel_y
+        # Do not simulate if ball is disabled
+        if ball.disabled:
+            continue
+
+        # Paddle-ball collision detection
+        if ball.rect.collidelist([paddle_a,paddle_b]) != -1:
+            if not ball.collided:
+                ball.vel_x *= -1
+                ball.collided = True
+        else:
+            ball.collided = False
+        
+        # Test for boundary collision
         if ball.pos_y >= SCREEN_HEIGHT - ball.radius or ball.pos_y < ball.radius:
             ball.vel_y *= -1
         if ball.pos_x >= SCREEN_WIDTH - ball.radius or ball.pos_x < ball.radius:
-            ball.vel_x *= -1
+            ball.disabled = True
 
-        # Paddle-ball collision detection
-        if paddle_a.pos_y == ball.pos_y and paddle_a.pos_x == ball.pos_x:
-            ball.vel_y *= -1
+        # Move the ball
+        ball.pos_x += ball.vel_x
+        ball.pos_y += ball.vel_y
 
         # Update positions
         ball.update()
