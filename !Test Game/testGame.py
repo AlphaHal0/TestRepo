be43653dea819da321d62a5b384f391381aec456
BagelDog <bageldogm@gmail.com>
2023-12-10 23:27:53 +0000

    Added a death screen and restarting. Cleaned up text class.

--- !Test Game/testGame.py
+++ !Test Game/testGame.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import *
-from classes import Player, ObstacleHandler
+from classes import Player, ObstacleHandler, Text
 from functions import core_functions
 
 # Set fundamentals
@@ -12,37 +12,52 @@ font = pygame.font.Font(None, 256)
 # Declare game stuff
 player = Player(200, 200, 20, 20, screen)
 obstacle_handler = ObstacleHandler(screen)
+scene = "GAME"
 
+# Main runtime
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and scene == "DEAD":
+            # If enter key is pressed and you are dead restart the game.
+            player = Player(200, 200, 20, 20, screen)
+            obstacle_handler = ObstacleHandler(screen)
+            scene = "GAME"
+
 
     core_functions(screen, clock, FPS)
-    
-    # Handle key presses
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_DOWN]:
-        player.move("down")
-    if keys[pygame.K_UP]:
-        player.move("up")
-    if keys[pygame.K_LEFT]:
-        player.move("left")
-    if keys[pygame.K_RIGHT]:
-        player.move("right")
-    
-    # Draw the obstacle
-    obstacle_handler.move_obstacles()
-    obstacle_handler.display_obstacles()
+    if scene == "GAME":
+        # Handle key presses
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]:
+            player.move("down")
+        if keys[pygame.K_UP]:
+            player.move("up")
+        if keys[pygame.K_LEFT]:
+            player.move("left")
+        if keys[pygame.K_RIGHT]:
+            player.move("right")
+        
+        # Draw the obstacle
+        obstacle_handler.move_obstacles()
+        obstacle_handler.display_obstacles()
 
-    # Check for collisions
-    if pygame.Rect.collidelist(player.rect, obstacle_handler.obstacles) != -1:
-        print("Collided") # Placeholder
-    
-    # Check for border collision and display the player
-    player.border_collision_check()
-    player.display()
+        # Check for collisions
+        if pygame.Rect.collidelist(player.rect, obstacle_handler.obstacles) != -1:
+            scene = "DEAD"
+            continue
+        
+        # Check for border collision and display the player
+        player.border_collision_check()
+        player.display()
+    elif scene == "DEAD":
+        text = Text(screen, (255, 0, 0), 45)
+        text.display("you died", 100, 100)
+        text.display("you are worthless", 100, 200)
+        text.display("enter to restart", 100, 300)
     
     # Update the display
     pygame.display.update()
